dba01d2d1783afd99db2044c10818c4106caa78e
Prime-victor <victorkiptum85@gmail.com>
2026-04-21 12:59:31 +0300

    feat: add root backend url routing

---
+++ backend/smartseason/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import include, path
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/auth/", include("apps.users.urls")),
+    path("api/", include("apps.fields.urls")),
+    path("api/dashboard/", include("apps.dashboard.urls")),
+]
