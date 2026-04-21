146b6c79047d6bc2d0454dd510ea96b7f6ed2f8b
Prime-victor <victorkiptum85@gmail.com>
2026-04-21 12:59:35 +0300

    feat: add user auth serializers

---
+++ backend/apps/users/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "name", "email", "role"]
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ["id", "name", "email", "password", "role"]
+        read_only_fields = ["id"]
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class SmartSeasonTokenSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["name"] = user.name
+        token["role"] = user.role
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data["user"] = UserSerializer(self.user).data
+        return data
