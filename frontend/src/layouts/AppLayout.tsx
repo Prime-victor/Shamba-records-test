6c10cea5dc709a774e67e819bfcf36af7791e22d
Prime-victor <victorkiptum85@gmail.com>
2026-04-21 12:59:49 +0300

    feat: add application layout shell

---
+++ frontend/src/layouts/AppLayout.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+
+import { Navbar } from "../components/Navbar";
+import { Sidebar } from "../components/Sidebar";
+
+export function AppLayout() {
+  return (
+    <div className="min-h-screen md:flex">
+      <Sidebar />
+      <main className="flex-1 bg-field-grid bg-[size:22px_22px] px-4 py-6 md:px-8 md:py-8">
+        <Navbar />
+        <Outlet />
+      </main>
+    </div>
+  );
+}
